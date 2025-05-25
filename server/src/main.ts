7a751e34da79c08fc3194d994f97e5682eae041f
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-05-25 21:46:09 +0200

    first commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,17 +5,23 @@ import "dotenv/config";
 // Note: This is optional and can be removed if the database connection
 // is not required when starting the application
 import "../database/checkConnection";
-
+import type { RequestHandler } from "express";
 // Import the Express application from ./app
 import app from "./app";
 
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
-
+const sayWelcome: RequestHandler = (_req, res) => {
+  res.send("Welcome to Wilder Series");
+};
 // Start the server and listen on the specified port
 app
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);
+
+    if (port === "3310") {
+      app.get("/", sayWelcome);
+    }
   })
   .on("error", (err: Error) => {
     console.error("Error:", err.message);
